4d4a4cab7be5c53436f196bf86dae7abe75191c0
Mishasya <mishasya@bk.ru>
2018-11-02 15:28:16 +0300

    Исправление замечаний

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 var mainNav = document.querySelector('.main-nav');
 var triggerNav = document.querySelector('.main-nav__trigger');
+var pageHeader = document.querySelector('.page-header__wrapper');
 
 mainNav.classList.remove('main-nav--nojs');
 
@@ -8,8 +9,14 @@ triggerNav.addEventListener('click', function(evt) {
     if (mainNav.classList.contains('main-nav--closed')) {
       mainNav.classList.remove('main-nav--closed');
       mainNav.classList.add('main-nav--opened');
+      pageHeader.classList.contains('page-header__wrapper--opened');
+      pageHeader.classList.remove('page-header__wrapper--opened');
+      pageHeader.classList.add('page-header__wrapper--closed');
+
     } else {
       mainNav.classList.add('main-nav--closed');
       mainNav.classList.remove('main-nav--opened');
+      pageHeader.classList.add('page-header__wrapper--opened');
+      pageHeader.classList.remove('page-header__wrapper--closed');
     }
   });
