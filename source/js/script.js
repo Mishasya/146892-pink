1e21ebddcf59f5e9c288180be4c78ddbd445e051
Mishasya <mishasya@bk.ru>
2018-11-06 09:52:02 +0300

    Исправления замечаний

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,22 @@
-var mainNav = document.querySelector('.main-nav');
-var triggerNav = document.querySelector('.main-nav__trigger');
-var pageHeader = document.querySelector('.page-header__wrapper');
+var mainNav = document.querySelector(".main-nav");
+var triggerNav = document.querySelector(".main-nav__trigger");
+var pageHeader = document.querySelector(".page-header__wrapper");
 
-mainNav.classList.remove('main-nav--nojs');
+mainNav.classList.remove("main-nav--nojs");
 
-triggerNav.addEventListener('click', function(evt) {
+triggerNav.addEventListener("click", function(evt) {
     evt.preventDefault();
-    if (mainNav.classList.contains('main-nav--closed')) {
-      mainNav.classList.remove('main-nav--closed');
-      mainNav.classList.add('main-nav--opened');
-      pageHeader.classList.contains('page-header__wrapper--opened');
-      pageHeader.classList.remove('page-header__wrapper--opened');
-      pageHeader.classList.add('page-header__wrapper--closed');
+    if (mainNav.classList.contains("main-nav--closed")) {
+      mainNav.classList.remove("main-nav--closed");
+      mainNav.classList.add("main-nav--opened");
+      pageHeader.classList.contains("page-header__wrapper--opened");
+      pageHeader.classList.remove("page-header__wrapper--opened");
+      pageHeader.classList.add("page-header__wrapper--closed");
 
     } else {
-      mainNav.classList.add('main-nav--closed');
-      mainNav.classList.remove('main-nav--opened');
-      pageHeader.classList.add('page-header__wrapper--opened');
-      pageHeader.classList.remove('page-header__wrapper--closed');
+      mainNav.classList.add("main-nav--closed");
+      mainNav.classList.remove("main-nav--opened");
+      pageHeader.classList.add("page-header__wrapper--opened");
+      pageHeader.classList.remove("page-header__wrapper--closed");
     }
   });
